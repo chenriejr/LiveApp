15bf05ab10bfa28ce08645fc4db15350328fe06e
Trace Pomplun <tracepomplun@gmail.com>
2015-05-19 09:19:04 -0500

    added MainViewController

---
+++ LiveApp/MainViewController.swift
@@ -0,0 +1,19 @@
+//
+//  MainViewController.swift
+//  LiveApp
+//
+//  Created by Trace Pomplun on 5/19/15.
+//  Copyright (c) 2015 Trace Pomplun. All rights reserved.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+
+    @IBOutlet weak var tableView: UITableView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+}
